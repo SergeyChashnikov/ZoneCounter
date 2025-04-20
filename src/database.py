500c573d32ee0a437fbf213d46cf60eaf4dc30b3
SergeyChashnikov <chashserq@gmail.com>
2025-04-20 20:58:14 +0300

    Очистка ветки main

--- src/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_ECHO
-
-# Создаем движок SQLAlchemy
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=SQLALCHEMY_ECHO)
-
-# Создаем фабрику сессий
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
